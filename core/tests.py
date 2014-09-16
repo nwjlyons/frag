aa00734f14dac34d67b6652de5a6e776255125c7
Neil Lyons <nwjlyons@googlemail.com>
2014-09-16 19:14:50 +0900

    Finished business logic.

--- core/tests.py
+++ core/tests.py
@@ -1,18 +1,53 @@
 from test_utils import TestSelenium
 
+from views import jquery, script, style
 
-class TestAUser(TestSelenium):
+
+class TestFrag(TestSelenium):
 
     # show_browser = True
 
-    def test_can_frag_a_webpage_and_update_the_url_fragment_by_clicking(self):
+    def test_frag_a_webpage_and_update_the_url_fragment_by_clicking(self):
         self.selenium.get(self.live_server_url)
 
         self.selenium.find_element_by_css_selector("[type=submit]").click()
         self.assertIn("This field is required.", self.selenium.page_source)
 
         url_input = self.selenium.find_element_by_name("url")
+        url_input.clear()
         url_input.send_keys('this is not a url')
-
         self.selenium.find_element_by_css_selector("[type=submit]").click()
         self.assertIn("Enter a valid URL.", self.selenium.page_source)
+
+        url_input = self.selenium.find_element_by_name("url")
+        url_input.clear()
+        url_input.send_keys('http://httpbin.org/status/404')
+        self.selenium.find_element_by_css_selector("[type=submit]").click()
+        self.assertIn("Error fetching page.", self.selenium.page_source)
+
+        url_input = self.selenium.find_element_by_name("url")
+        url_input.clear()
+        url_input.send_keys('http://en.wikipedia.org/wiki/Japan')
+        self.selenium.find_element_by_css_selector("[type=submit]").click()
+        self.assertEqual(self.selenium.title, u'Japan - Wikipedia, the free encyclopedia')
+        self.assertEqual(self.selenium.current_url,
+            u'{}/?url=http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJapan'.format(self.live_server_url))
+
+        self.assertIn(str(style), self.selenium.page_source)
+        # self.assertIn(str(jquery), self.selenium.page_source) # This fails for some reason
+        self.assertIn(str(script), self.selenium.page_source)
+
+
+        self.selenium.find_element_by_id("Culture").click()
+        self.assertEqual(self.selenium.current_url,
+            u'{}/?url=http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJapan#Culture'.format(self.live_server_url))
+
+        self.selenium.find_element_by_id("frag-2743").click()
+        self.assertEqual(self.selenium.current_url,
+            u'{}/?url=http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJapan#frag-2743'.format(self.live_server_url))
+
+    def test_page_doesnt_contain_style_and_script_tags_we_added(self):
+        self.selenium.get(self.live_server_url + "?url=http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJapan#frag-2743")
+
+        self.assertNotIn(str(style), self.selenium.page_source)
+        self.assertNotIn(str(script), self.selenium.page_source)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,30 @@
+from urlparse import urljoin
+
+from bs4 import BeautifulSoup
+from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.http import is_safe_url
+import requests
 
 from forms import FragForm
+
+jquery = BeautifulSoup('<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>')
+script = BeautifulSoup("""
+<script>
+$(function(){
+
+    $("[id]").click(function(event){
+        event.stopPropagation();
+        window.location.hash = $(this).attr("id");
+    })
+
+});
+</script>""")
+style = BeautifulSoup("""
+<style>
+[id]:hover {outline: 1px solid red;}
+</style>""")
 
 
 def home(request):
@@ -8,8 +32,35 @@ def home(request):
     if "url" in request.GET:
         form = FragForm(request.GET)
         if form.is_valid():
-            from django.http import HttpResponse
-            return HttpResponse("soup it")
+            url = form.cleaned_data["url"]
+            response = requests.get(url)
+            if response.ok:
+
+                soup = BeautifulSoup(response.content)
+
+                count = 0
+                for element in soup.select("body *"):
+                    if "id" not in element.attrs or element.attrs["id"] == "":
+                        element["id"] = "frag-%s" % count
+                    count += 1
+
+                def absoulte_urls(attribute_key):
+                    for element in soup.select("[%s]" % attribute_key):
+                        path = element.attrs[attribute_key]
+                        if not len([prefix for prefix in ["http", "https", "//"] if path.startswith(prefix)]):
+                            element.attrs[attribute_key] = urljoin(url, path)
+
+                absoulte_urls("src")
+                absoulte_urls("href")
+
+                if "HTTP_REFERER" in request.META and is_safe_url(request.META["HTTP_REFERER"], request.get_host()):
+                    soup.head.insert(len(soup.head.contents), style)
+                    soup.head.insert(len(soup.head.contents), jquery)
+                    soup.head.insert(len(soup.head.contents), script)
+
+                return HttpResponse(str(soup))
+            else:
+                messages.error(request, "Error fetching page.")
     else:
         form = FragForm()
 
